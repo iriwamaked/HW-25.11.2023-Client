d272a91bbed0d748caecf059aeab918a37b4bd09
Irina Makedonska <iriwamaked@gmail.com>
2023-12-01 15:30:06 +0200

    01.12.2023 Working Client

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net.Sockets;
+using System.IO;
 
 namespace Client
 {
@@ -22,94 +23,139 @@ namespace Client
     /// </summary>
     public partial class MainWindow : Window
     {
-        public class ClientSocket
+        public class Client
         {
-            public string adress { get; set; }
-            public short port { get; set; }
+            public string login { get; set; }
+            public string password { get; set; }
             public IPEndPoint point { get; set; }
             public StringBuilder sb { get; set; }
-            public Socket clientSocket { get; set; }
-            public ClientSocket(string adress, short port)
+            public TcpClient client { get; set; }
+
+            public NetworkStream stream { get; set; }
+            public Client(string login, string password)
             {
-                this.adress = adress;
-                this.port = port;
-                point=new IPEndPoint(IPAddress.Parse(adress), port);
-                clientSocket = new(point.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                this.login = login;
+                this.password = password;
+                point = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+                client = new TcpClient();
                 sb = new StringBuilder();
+               
             }
 
-            public async Task Connection()
+            public async Task SendCredentials()
             {
-                await clientSocket.ConnectAsync(point);
+                string credentialMessage = $"{login},{password}";
+                await SendMessage(credentialMessage);
+            }
+            public async Task<bool> Connection()
+            {
+                await client.ConnectAsync(point);
+                stream = client.GetStream();
+                await SendCredentials();
+                await GetMessage();
+                string serverMessage = sb.ToString().Trim();
+                if (serverMessage == "Later")
+                {
+                    MessageBox.Show("Cервер перевантажений. Спробуйте пізніше");
+                    return false;
+                }
+                else if (serverMessage == "InvalidCredentials")
+                {
+                    MessageBox.Show("Логін чи пароль зазначені не вірно. Спробуйте ще раз.");
+                    return false;
+                }
+                else
+                {
+                    MessageBox.Show("З'єднання успішно встановлено");
+                    return true;
+                }
             }
 
             public async Task GetMessage()
             {
                 sb.Clear();
-                var buffer=new byte[1024];
-                var received=await clientSocket.ReceiveAsync(buffer, SocketFlags.None);
-                sb.Append(Encoding.Unicode.GetString(buffer, 0, received));
+                var buffer = new byte[1024];
+                int bytes = 0;
+                do
+                {
+                    bytes = await stream.ReadAsync(buffer, 0, buffer.Length);
+                    sb.Append(Encoding.Unicode.GetString(buffer, 0, bytes));
+                }
+                while (stream.DataAvailable);
             }
-
             public async Task SendMessage(string message)
             {
-                if (clientSocket == null || !clientSocket.Connected)
+                if (client == null || !client.Connected)
                 {
-                    Console.WriteLine("Socket is not connected.");
+                    Console.WriteLine("Client is not connected.");
                     return;
                 }
 
                 byte[] data = Encoding.Unicode.GetBytes(message);
-                await clientSocket.SendAsync(data, SocketFlags.None);
+                await stream.WriteAsync(data, 0, data.Length);
             }
 
             public async Task EndConnection()
             {
-                clientSocket.Shutdown(SocketShutdown.Both);
-                clientSocket.Close();
+                client.Close();
             }
-            
+
         }
 
-        ClientSocket cS = null;
+        Client cS = null;
         public MainWindow()
         {
             InitializeComponent();
-            Messaging.Visibility= Visibility.Hidden;
+            Messaging.Visibility = Visibility.Hidden;
         }
 
-        private async void ConnectUser_Click(object sender, RoutedEventArgs e)
-        {
-            if (!string.IsNullOrEmpty(IpAdress.Text) && !string.IsNullOrEmpty(Port.Text))
-            {
-                if(short.TryParse(Port.Text, out short port))
-                {
-                    cS = new ClientSocket(IpAdress.Text, port);
-                    await cS.Connection();
-                    MessageBox.Show("З'єднання успішно встановлено");
-                    ConnectionGrid.Visibility = Visibility.Hidden;
-                }
-                else
-                {
-                    MessageBox.Show("Порт має бути цілим числом");
-                }
-                
-            }
-            else
-            {
-                MessageBox.Show("IP-адреса та порт повині бути заповнені. \n" +
-                    "Якщо у вас є складнощі з заповненням, скористайтесь кнопкою для приєднання із параметрами за замовчуванням");
-            }
-            
-        }
+        //private async void ConnectUser_Click(object sender, RoutedEventArgs e)
+        //{
+        //    if (!string.IsNullOrEmpty(IpAdress.Text) && !string.IsNullOrEmpty(Port.Text))
+        //    {
+        //        if(short.TryParse(Port.Text, out short port))
+        //        {
+        //            cS = new Client(IpAdress.Text, port);
+        //            if(await cS.Connection() == true)
+        //            {
+        //                ConnectionGrid.Visibility = Visibility.Hidden;
+        //                Messaging.Visibility = Visibility.Visible;
+        //            }
+
+        //        }
+        //        else
+        //        {
+        //            MessageBox.Show("Порт має бути цілим числом");
+        //        }           
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("IP-адреса та порт повині бути заповнені. \n" +
+        //            "Якщо у вас є складнощі з заповненням, скористайтесь кнопкою для приєднання із параметрами за замовчуванням");
+        //    }
+
+        //}
 
         private async void ConnectDefaul_Click(object sender, RoutedEventArgs e)
         {
-            cS = new ClientSocket("127.0.0.1", 8080);
-            await cS.Connection();
-            MessageBox.Show("З'єднання успішно встановлено");
-            ConnectionGrid.Visibility = Visibility.Hidden;
-            Messaging.Visibility = Visibility.Visible;
+            if (!string.IsNullOrEmpty(LoginText.Text) && !string.IsNullOrEmpty(PasswordText.Text))
+            {
+                cS = new Client(LoginText.Text, PasswordText.Text);
+                if (await cS.Connection() == true)
+                {
+                    ConnectionGrid.Visibility = Visibility.Hidden;
+                    Messaging.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    MessageBox.Show("Cервер відхилив з'єднання. Логін або пароль не вірні. Спробуйте ще раз");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Логін або пароль не зазначені");
+            }
+
         }
 
         private async void UserSendMessage_Click(object sender, RoutedEventArgs e)
@@ -118,29 +164,58 @@ namespace Client
             {
                 await SendMessageAndReceiveResponse();
             }
-            catch (Exception ex) 
-            { 
+            catch (Exception ex)
+            {
                 MessageBox.Show(ex.Message);
             }
-            
+
         }
 
         private async Task SendMessageAndReceiveResponse()
         {
-            await cS.SendMessage(UserMessage.Text);
+            await WriteMessageAsync(cS.stream, UserMessage.Text);
+
+            if (UserMessage.Text.ToLower() == "bye")
+            {
+                await EndMessaging();
+                return;
+            }
+
             UserMessage.Text = string.Empty;
 
-            await cS.GetMessage();
-            ServerMessage.Text = cS.sb.ToString();
-            if (ServerMessage.Text=="bye")
+            ServerMessage.Text = await ReadMessageAsync(cS.stream);
+
+            if (ServerMessage.Text.ToLower() == "bye")
             {
-               await EndMessaging();
+                await EndMessaging();
             }
+        }
+
+        public static async Task<string> ReadMessageAsync(NetworkStream stream)
+        {
+            byte[] data = new byte[1024];
+            StringBuilder sb = new StringBuilder();
+            int bytes = 0;
+
+            do
+            {
+                bytes = await stream.ReadAsync(data, 0, data.Length);
+                sb.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            }
+            while (stream.DataAvailable);
+
+            return sb.ToString();
+        }
+
+        public static async Task WriteMessageAsync(NetworkStream stream, string message)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            await stream.WriteAsync(data, 0, data.Length);
         }
 
         private async void UserBye_Click(object sender, RoutedEventArgs e)
         {
-           try
+            try
             {
                 await ByeUserMessage();
             }
